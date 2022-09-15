1a702af18ed108e216011064e983949be9f29a5a
Vitor-tml <vitor.gabriel.vinielle@gmail.com>
2022-09-15 18:20:57 -0300

    adicionando ex 006

--- lista-estruturas-de-repeticao-2/ex006.c
+++ lista-estruturas-de-repeticao-2/ex006.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <time.h>
 
-
 int main()
 {
     srand(time(NULL));
-    int number = rand()%500 + 1;
-    printf("Hello Word!");
+    int number = rand() % 500 + 1;
+    int try, attempts = 1;
+
+    //printf("%d\n", number);
+    printf("Try to guess the value.\n");
+    printf("Enter the value: ");
+    scanf("%d", &try);
+
+    while (try != number)
+    {
+        attempts++;
+        printf("Enter the value: ");
+        scanf("%d", &try);
+    }
+    printf("You`re right!");
+
+    if (attempts <= 3)
+        printf("\\o/");
+    else 
+        if (attempts <= 10)
+            printf(":-D");
+        else
+            printf(":-\\");
+
     return 0;
 }
