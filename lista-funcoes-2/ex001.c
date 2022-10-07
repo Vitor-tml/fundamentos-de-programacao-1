5e94c85c9afa0cd89658dc4c82b1a257adcd8c29
Vitor Silva <vitor.gabriel.vinielle@gmail.com>
2022-10-07 14:26:56 -0300

    adicionando ex01 da lista de funçôes 2

---
+++ lista-funcoes-2/ex001.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void questao01(int *a, int *b);
+int main()
+{
+    int a, b;
+
+    printf("Entre com a e b: ");
+    scanf("%d %d", &a, &b);
+
+    questao01(&a, &b);
+
+    printf(" a = %d b = %d\n", a, b);
+    return 0;
+}
+void questao01(int *a, int *b)
+{
+    ++*a;
+    --*b;
+    return;
+}
