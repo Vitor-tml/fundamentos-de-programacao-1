58f3a5c5027ea29c78b8f0fa6719a38a3f0897ef
Vitor Silva <vitor.gabriel.vinielle@gmail.com>
2022-11-04 17:50:10 -0300

    iniciando ex004

--- lista-alocacao-dinamica/ex004.c
+++ lista-alocacao-dinamica/ex004.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include "../funcoes-basicas.c"
 
+int** alocaMatriz(int nl, int nc);
+
 int main()
 {
-    
+    int **m;
+    m = alocaMatriz(2,2);
+    preencheMatriz(m, 2, 2, 0, 4);
+    imprimeMatriz(m, 2, 2);
+}
+
+int** alocaMatriz(int nl, int nc)
+{
+    int i, **m;
+
+    for(i = 0; i < nl; i++)
+        m[i] = (int *) malloc(nc * sizeof(int *));
+    return m;
 }
